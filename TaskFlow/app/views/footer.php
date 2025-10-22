79abb4ea68a6ec591dcd2717420c568b3db341c5
Pedro Perez Fernandez <pedroperezfernandez1505@gmail.com>
2025-10-22 13:29:17 +0200

    Update footer.php

--- TaskFlow/app/views/footer.php
+++ TaskFlow/app/views/footer.php
@@ -1,4 +1,5 @@
- <h2>Perfil de usuario</h2>
+<?php ?>
+<h2>Perfil de usuario</h2>
             <p><strong>Nombre:</strong> <?php echo $userName; ?></p>
             <p><strong>Edad:</strong> <?php echo $userAge; ?></p>
             <p><strong>Estado de la cuenta:</strong>Usuario<?php echo $isPremiunUser ? ' Premium' : ' Estándar'; ?></p>
