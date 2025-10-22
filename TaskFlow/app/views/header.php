02a88e9df8fc2c8fce10436d231f14933fc6620e
Pedro Perez Fernandez <pedroperezfernandez1505@gmail.com>
2025-10-22 12:48:23 +0200

    Update header.php

--- TaskFlow/app/views/header.php
+++ TaskFlow/app/views/header.php
@@ -9,10 +9,3 @@
             <h1>Bienvenido a <?php echo SITE_NAME;?></h1>
         </header>
         <main>
-            <h2>Perfil de usuario</h2>
-            <p><strong>Nombre:</strong> <?php echo $userName; ?></p>
-            <p><strong>Edad:</strong> <?php echo $userAge; ?></p>
-            <p><strong>Estado de la cuenta:</strong>Usuario<?php echo $isPremiunUser ? ' Premium' : ' Estándar'; ?></p>
-        </main>
-    </body>
-</html>
